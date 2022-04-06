f6742bc99930eaa89d9dfef26501f5262c4afa54
Chris Pilsworth <chrisp@adobe.com>
2022-04-06 10:35:10 +0100

    wip styling for featured-article

---
+++ blocks/featured-article/featured-article.js
@@ -0,0 +1,10 @@
+import { readBlockConfig } from '../../scripts/scripts.js';
+
+/**
+ * loads and decorates the footer
+ * @param {Element} block The header block element
+ */
+
+export default async function decorate(block) {
+    const cfg = readBlockConfig(block);
+}
