e79e993f97a49d8911dfaf3f68dd8a31cefd5588
Chris Pilsworth <chrisp@adobe.com>
2022-05-05 12:56:24 +0100

    remove old code - call github api

--- blocks/oauth/oauth.js
+++ blocks/oauth/oauth.js
@@ -1,45 +1,10 @@
-const WORKER_URL = "https://worker.cpilsworth.workers.dev"
-const code = new URL(window.location.href).searchParams.get("code")
-const $login = document.querySelector(".oauth")
-
-async function login(code) {
-    // remove ?code=... from URL
-    const { history } = window
-    const { location } = history
-
-    const path =
-        location.pathname +
-        location.search.replace(/\bcode=\w+/, "").replace(/\?$/, "")
-    history.pushState({}, "", path)
-
-    document.body.dataset.state = "loading"
-
-    const response = await fetch(WORKER_URL, {
-        method: "POST",
-        mode: "cors",
-        headers: {
-            "content-type": "application/json",
-        },
-        body: JSON.stringify({ code }),
-    })
-
-    const result = await response.json()
-
-    if (result.error) {
-        return alert(JSON.stringify(result, null, 2))
-    }
-
-    // token can now be used to send authenticated requests against https://api.github.com
-    const getUserResponse = await fetch("https://api.github.com/user", {
+async function getUser(token) {
+    return fetch("https://api.github.com/user", {
         headers: {
             accept: "application/vnd.github.v3+json",
-            authorization: `token ${result.token}`,
+            authorization: `token ${token}`,
         },
-    })
-    const { login } = await getUserResponse.json()
-    $login.textContent = login
-    document.body.dataset.state = "signed-in"
-    return Promise.resolve("ok")
+    }).then((res) => res.json())
 }
 
 function getCookies() {
@@ -51,6 +16,12 @@ function getCookies() {
     }, {})
 }
 
-if (code) {
-    login(code)
+async function showUser() {
+    const { authtoken } = getCookies()
+    const user = await getUser(authtoken)
+    document.getElementsByClassName("oauth")[0].innerHTML = `${JSON.stringify(
+    user,
+    null,
+    2,
+  )}`
 }
