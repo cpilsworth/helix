f5e09add771197c52d274262d3be6c7d01976de2
Chris Pilsworth <chrisp@adobe.com>
2022-05-25 18:20:11 +0100

    latest header code - make links relative

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,4 +1,4 @@
-import { readBlockConfig } from '../../scripts/scripts.js';
+import { readBlockConfig, decorateIcons, makeLinksRelative } from '../../scripts/scripts.js';
 
 /**
  * collapses all open nav sections
@@ -6,9 +6,9 @@ import { readBlockConfig } from '../../scripts/scripts.js';
  */
 
 function collapseAllNavSections(sections) {
-  sections.querySelectorAll('.nav-section').forEach((section) => {
-    section.setAttribute('aria-expanded', 'false');
-  });
+    sections.querySelectorAll('.nav-sections > ul > li').forEach((section) => {
+        section.setAttribute('aria-expanded', 'false');
+    });
 }
 
 /**
@@ -17,53 +17,51 @@ function collapseAllNavSections(sections) {
  */
 
 export default async function decorate(block) {
-  const cfg = readBlockConfig(block);
-  block.textContent = '';
+    const cfg = readBlockConfig(block);
+    block.textContent = '';
 
-  // fetch nav content
-  const navPath = cfg.nav || '/nav';
-  const resp = await fetch(`${navPath}.plain.html`);
-  const html = await resp.text();
+    // fetch nav content
+    const navPath = cfg.nav || '/nav';
+    const resp = await fetch(`${navPath}.plain.html`);
+    if (resp.ok) {
+        const html = await resp.text();
 
-  // decorate nav DOM
-  const nav = document.createElement('div');
-  nav.classList.add('nav');
-  nav.setAttribute('aria-role', 'navigation');
-  const navSections = document.createElement('div');
-  navSections.classList.add('nav-sections');
-  nav.innerHTML = html;
-  nav.querySelectorAll(':scope > div').forEach((navSection, i) => {
-    if (!i) {
-      // first section is the brand section
-      const brand = navSection;
-      brand.classList.add('nav-brand');
-    } else {
-      // all other sections
-      navSections.append(navSection);
-      navSection.classList.add('nav-section');
-      const h2 = navSection.querySelector('h2');
-      if (h2) {
-        h2.addEventListener('click', () => {
-          const expanded = navSection.getAttribute('aria-expanded') === 'true';
-          collapseAllNavSections(navSections);
-          navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+        // decorate nav DOM
+        const nav = document.createElement('nav');
+        nav.innerHTML = html;
+        decorateIcons(nav);
+        makeLinksRelative(nav);
+
+        const classes = ['brand', 'sections', 'tools'];
+        classes.forEach((e, j) => {
+            const section = nav.children[j];
+            if (section) section.classList.add(`nav-${e}`);
         });
-      }
+
+        const navSections = [...nav.children][1];
+        if (navSections) {
+            navSections.querySelectorAll(':scope > ul > li').forEach((navSection) => {
+                if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
+                navSection.addEventListener('click', () => {
+                    const expanded = navSection.getAttribute('aria-expanded') === 'true';
+                    collapseAllNavSections(navSections);
+                    navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+                });
+            });
+        }
+
+        // hamburger for mobile
+        const hamburger = document.createElement('div');
+        hamburger.classList.add('nav-hamburger');
+        hamburger.innerHTML = '<div class="nav-hamburger-icon"></div>';
+        hamburger.addEventListener('click', () => {
+            const expanded = nav.getAttribute('aria-expanded') === 'true';
+            document.body.style.overflowY = expanded ? '' : 'hidden';
+            nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+        });
+        nav.prepend(hamburger);
+        nav.setAttribute('aria-expanded', 'false');
+        decorateIcons(nav);
+        block.append(nav);
     }
-  });
-  nav.append(navSections);
-
-  // hamburger for mobile
-  const hamburger = document.createElement('div');
-  hamburger.classList.add('nav-hamburger');
-  hamburger.innerHTML = '<div class="nav-hamburger-icon"></div>';
-  hamburger.addEventListener('click', () => {
-    const expanded = nav.getAttribute('aria-expanded') === 'true';
-    document.body.style.overflowY = expanded ? '' : 'hidden';
-    nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
-  });
-  nav.prepend(hamburger);
-  nav.setAttribute('aria-expanded', 'false');
-
-  block.append(nav);
 }
